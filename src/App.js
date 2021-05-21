c4ab71f8123ce8436f8a7ed9fd7ab9958bdad9b5
Alexandra <arbeuerman@gmail.com>
2021-05-21 10:06:05 -0500

    commit tree 40dd592df23e0ea4d10d84a89918db747936a25c

--- src/App.js
+++ src/App.js
@@ -4,26 +4,21 @@ import TwitterMessage from "./components/TwitterMessage";
 
 class App extends Component {
 
-  login = ({ username, password }) => {
+  login = ({username, password}) => {
     console.log(`Logging in ${username} with password ${password}`);
   };
 
   render() {
     return (
       <div>
-
         <h1>
           <pre>LoginForm</pre>
         </h1>
         <LoginForm handleLogin={this.login} />
-
         <h1>
           <pre>TwitterMessage</pre>
         </h1>
         <TwitterMessage maxChars={280} />
-
-
-
       </div>
     )
   }
